6b8ef005060162995dbb91cb09e7df84fcff2242
James Pettit <james@academia.edu>
2020-06-02 17:41:17 +0800

    Swap out torus for custom triangle mesh.

--- marching_cubes.go
+++ marching_cubes.go
@@ -15,17 +15,54 @@ import (
 	"github.com/g3n/engine/renderer"
 )
 
+func flatten(l []*math32.Vector3) math32.ArrayF32 {
+	a := math32.NewArrayF32(3*len(l), 3*len(l))
+	for i, vec := range l {
+		a[i*3] = vec.X
+		a[i*3+1] = vec.Y
+		a[i*3+2] = vec.Z
+	}
+	return a
+}
+
+func computeNormals(vertices []*math32.Vector3) math32.ArrayF32 {
+	normals := math32.NewArrayF32(len(vertices), len(vertices))
+	for i := 0; i < len(vertices)/3; i++ {
+		n := math32.Normal(vertices[i*3], vertices[i*3+1], vertices[i*3+2], nil)
+		normals[i*3] = n.X
+		normals[i*3+1] = n.Y
+		normals[i*3+2] = n.Z
+	}
+	return normals
+}
+
+func indices(vertices []*math32.Vector3) math32.ArrayU32 {
+	indices := math32.NewArrayU32(len(vertices), len(vertices))
+	for i := range vertices {
+		indices[i] = uint32(i)
+	}
+	return indices
+}
+
 func main() {
 
 	a := app.App()
-
-	// Create a blue torus and add it to the scene
-	geom := geometry.NewTorus(1, .4, 12, 32, math32.Pi*2)
-	mat := material.NewStandard(math32.NewColor("DarkBlue"))
-	torusMesh := graphic.NewMesh(geom, mat)
-
 	scene := core.NewNode()
-	scene.Add(torusMesh)
+
+	// Create a mesh and add it to the scene
+	geom := geometry.NewGeometry()
+	vertices := []*math32.Vector3{
+		{-1, -1, 0},
+		{1, -1, 0},
+		{0, 1, 0},
+	}
+	geom.AddVBO(gls.NewVBO(flatten(vertices)).AddAttrib(gls.VertexPosition))
+	geom.AddVBO(gls.NewVBO(computeNormals(vertices)).AddAttrib(gls.VertexNormal))
+	geom.SetIndices(indices(vertices))
+	mat := material.NewStandard(math32.NewColor("Green"))
+	mat.SetSide(material.SideDouble)
+	mesh := graphic.NewMesh(geom, mat)
+	scene.Add(mesh)
 
 	// Lights
 	ambientLight := light.NewAmbient(&math32.Color{1.0, 1.0, 1.0}, 0.8)
@@ -37,6 +74,7 @@ func main() {
 	// Camera
 	cam := camera.New(1)
 	cam.SetPosition(0, 0, 10)
+	camera.NewOrbitControl(cam)
 	scene.Add(cam)
 
 	width, height := a.GetFramebufferSize()
