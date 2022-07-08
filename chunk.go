26a4f15242c56db2fbd70674f0d9c9abf5ca63be
James Pettit <etherealmachine@gmail.com>
2022-07-08 08:57:41 -0700

    Greedy meshing not quite working

--- chunk.go
+++ chunk.go
@@ -165,9 +165,7 @@ func (c *Chunk) interior(x, y, z int) bool {
 }
 
 func (c *Chunk) CulledGeom() geometry.IGeometry {
-	var positions []float32
-	var uvs []float32
-	var indices []uint32
+	g := &GeometryBuilder{}
 	for x := 0; x < ChunkSize; x++ {
 		for y := 0; y < ChunkSize; y++ {
 			for z := 0; z < ChunkSize; z++ {
@@ -176,102 +174,56 @@ func (c *Chunk) CulledGeom() geometry.IGeometry {
 					yf := float32(y)
 					zf := float32(z)
 
-					i := uint32(len(positions) / 3)
+					i := g.CurrentTriangleIndex()
 					// 0, 0, 0
-					positions = append(positions, xf)
-					positions = append(positions, yf)
-					positions = append(positions, zf)
+					g.AddVertex(xf, yf, zf)
 					// 0, 0, 1
-					positions = append(positions, xf)
-					positions = append(positions, yf)
-					positions = append(positions, zf+1)
+					g.AddVertex(xf, yf, zf+1)
 					// 1, 0, 0
-					positions = append(positions, xf+1)
-					positions = append(positions, yf)
-					positions = append(positions, zf)
+					g.AddVertex(xf+1, yf, zf)
 					// 1, 0, 1
-					positions = append(positions, xf+1)
-					positions = append(positions, yf)
-					positions = append(positions, zf+1)
+					g.AddVertex(xf+1, yf, zf+1)
 					// 0, 1, 0
-					positions = append(positions, xf)
-					positions = append(positions, yf+1)
-					positions = append(positions, zf)
+					g.AddVertex(xf, yf+1, zf)
 					// 0, 1, 1
-					positions = append(positions, xf)
-					positions = append(positions, yf+1)
-					positions = append(positions, zf+1)
+					g.AddVertex(xf, yf+1, zf+1)
 					// 1, 1, 0
-					positions = append(positions, xf+1)
-					positions = append(positions, yf+1)
-					positions = append(positions, zf)
+					g.AddVertex(xf+1, yf+1, zf)
 					// 1, 1, 1
-					positions = append(positions, xf+1)
-					positions = append(positions, yf+1)
-					positions = append(positions, zf+1)
+					g.AddVertex(xf+1, yf+1, zf+1)
 
 					// bottom
-					indices = append(indices, i+0)
-					indices = append(indices, i+2)
-					indices = append(indices, i+1)
-					indices = append(indices, i+1)
-					indices = append(indices, i+2)
-					indices = append(indices, i+3)
+					g.AddTriangle(i+0, i+2, i+1)
+					g.AddTriangle(i+1, i+2, i+3)
 
 					// top
-					indices = append(indices, i+4)
-					indices = append(indices, i+5)
-					indices = append(indices, i+6)
-					indices = append(indices, i+6)
-					indices = append(indices, i+5)
-					indices = append(indices, i+7)
+					g.AddTriangle(i+4, i+5, i+6)
+					g.AddTriangle(i+6, i+5, i+7)
 
 					// front
-					indices = append(indices, i+5)
-					indices = append(indices, i+1)
-					indices = append(indices, i+7)
-					indices = append(indices, i+7)
-					indices = append(indices, i+1)
-					indices = append(indices, i+3)
+					g.AddTriangle(i+5, i+1, i+7)
+					g.AddTriangle(i+7, i+1, i+3)
 
 					// back
-					indices = append(indices, i+4)
-					indices = append(indices, i+6)
-					indices = append(indices, i+0)
-					indices = append(indices, i+0)
-					indices = append(indices, i+6)
-					indices = append(indices, i+2)
+					g.AddTriangle(i+4, i+6, i+0)
+					g.AddTriangle(i+0, i+6, i+2)
 
 					// left
-					indices = append(indices, i+0)
-					indices = append(indices, i+1)
-					indices = append(indices, i+4)
-					indices = append(indices, i+4)
-					indices = append(indices, i+1)
-					indices = append(indices, i+5)
+					g.AddTriangle(i+0, i+1, i+4)
+					g.AddTriangle(i+4, i+1, i+5)
 
 					// right
-					indices = append(indices, i+3)
-					indices = append(indices, i+2)
-					indices = append(indices, i+7)
-					indices = append(indices, i+7)
-					indices = append(indices, i+2)
-					indices = append(indices, i+6)
+					g.AddTriangle(i+3, i+2, i+7)
+					g.AddTriangle(i+7, i+2, i+6)
 				}
 			}
 		}
 	}
-	normals := make([]float32, len(positions))
-	geom := geometry.NewGeometry()
-	geom.AddVBO(gls.NewVBO(positions).AddAttrib(gls.VertexPosition))
-	normals = geometry.CalculateNormals(indices, positions, normals)
-	geom.AddVBO(gls.NewVBO(normals).AddAttrib(gls.VertexNormal))
-	geom.AddVBO(gls.NewVBO(uvs).AddAttrib(gls.VertexTexcoord))
-	geom.SetIndices(indices)
-	return geom
+	return g.Build()
 }
 
 func (c *Chunk) GreedyGeom() geometry.IGeometry {
+	g := &GeometryBuilder{}
 	// Sweep over each axis (X, Y and Z)
 	for d := 0; d < 3; d++ {
 		u := (d + 1) % 3
@@ -289,11 +241,15 @@ func (c *Chunk) GreedyGeom() geometry.IGeometry {
 			for x[v] = 0; x[v] < ChunkSize; x[v]++ {
 				for x[u] = 0; x[u] < ChunkSize; x[u]++ {
 					// q determines the direction (X, Y or Z) that we are searching
-					blockCurrent := c.data[x[0]][x[1]][x[2]]
-					blockCompare := c.data[x[0]+q[0]][x[1]+q[1]][x[2]+q[2]]
 					// The mask is set to true if there is a visible face between two blocks,
 					//   i.e. both aren't empty and both aren't blocks
-					mask[n] = blockCurrent != blockCompare
+					if x[0] < 0 || x[1] < 0 || x[2] < 0 || x[0] >= ChunkSize || x[1] >= ChunkSize || x[2] >= ChunkSize || x[0]+q[0] < 0 || x[1]+q[1] < 0 || x[2]+q[2] < 0 || x[0]+q[0] >= ChunkSize || x[1]+q[1] >= ChunkSize || x[2]+q[2] >= ChunkSize {
+						mask[n] = true
+					} else {
+						blockCurrent := c.data[x[0]][x[1]][x[2]]
+						blockCompare := c.data[x[0]+q[0]][x[1]+q[1]][x[2]+q[2]]
+						mask[n] = blockCurrent != blockCompare
+					}
 					n++
 				}
 			}
@@ -318,19 +274,14 @@ func (c *Chunk) GreedyGeom() geometry.IGeometry {
 						//   For example, if w is 5 we currently have a quad of dimensions 1 x 5. To reduce triangle count,
 						//   greedy meshing will attempt to expand this quad out to CHUNK_SIZE x 5, but will stop if it reaches a hole in the mask
 
-						done := false
-						for ; j+h < ChunkSize; h++ {
+						for hole := false; j+h < ChunkSize && !hole; h++ {
 							// Check each block next to this quad
 							for k := 0; k < w; k++ {
 								// If there's a hole in the mask, exit
 								if !mask[n+k+h*ChunkSize] {
-									done = true
+									hole = true
 									break
 								}
-							}
-
-							if done {
-								break
 							}
 						}
 
@@ -344,14 +295,13 @@ func (c *Chunk) GreedyGeom() geometry.IGeometry {
 						var dv [3]int
 						dv[v] = h
 
-						/*
-						   // Create a quad for this face. Colour, normal or textures are not stored in this block vertex format.
-						   BlockVertex.AppendQuad(new Int3(x[0],                 x[1],                 x[2]),                 // Top-left vertice position
-						                          new Int3(x[0] + du[0],         x[1] + du[1],         x[2] + du[2]),         // Top right vertice position
-						                          new Int3(x[0] + dv[0],         x[1] + dv[1],         x[2] + dv[2]),         // Bottom left vertice position
-						                          new Int3(x[0] + du[0] + dv[0], x[1] + du[1] + dv[1], x[2] + du[2] + dv[2])  // Bottom right vertice position
-						                          );
-						*/
+						ti := g.CurrentTriangleIndex()
+						g.AddVertex(float32(x[0]), float32(x[1]), float32(x[2]))
+						g.AddVertex(float32(x[0]+du[0]), float32(x[1]+du[1]), float32(x[2]+du[2]))
+						g.AddVertex(float32(x[0]+dv[0]), float32(x[1]+dv[1]), float32(x[2]+dv[2]))
+						g.AddVertex(float32(x[0]+du[0]+dv[0]), float32(x[1]+du[1]+dv[1]), float32(x[2]+du[2]+dv[2]))
+						g.AddTriangle(ti+0, ti+2, ti+1)
+						g.AddTriangle(ti+1, ti+2, ti+3)
 
 						// Clear this part of the mask, so we don't add duplicate faces
 						for l := 0; l < h; l++ {
@@ -371,11 +321,12 @@ func (c *Chunk) GreedyGeom() geometry.IGeometry {
 			}
 		}
 	}
+	return g.Build()
 }
 
 func (c *Chunk) Mesh() graphic.IGraphic {
 	mat := material.NewStandard(math32.NewColor("white"))
 	mat.AddTexture(textures["dirt"])
 	mat.SetShader("terrain")
-	return graphic.NewMesh(c.CulledGeom(), mat)
+	return graphic.NewMesh(c.GreedyGeom(), mat)
 }
